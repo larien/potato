e9dd23832eed67c4248a151f97dd669ab8d52570
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-12-25 17:23:14 -0300

    feat: register GET Potato handler

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,5 +12,17 @@ const (
 func main() {
 	log.Println("starting server in port", port)
 
+	http.HandleFunc("/potato", v1GetPotato)
+
 	http.ListenAndServe(port, nil)
+}
+
+func v1GetPotato(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == http.MethodGet {
+		w.Write([]byte("{\"content\": \"potato\"}"))
+		return
+	}
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write([]byte("{\"error\": \"method not allowed\"}"))
 }
