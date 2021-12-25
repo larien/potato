3a8826300d9e03dd24165a33c44108c88cfc75aa
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-12-25 18:09:37 -0300

    refactor: create handler folder

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/larien/go-skelethon/handler"
 )
 
 const (
@@ -12,17 +14,7 @@ const (
 func main() {
 	log.Println("starting server in port", port)
 
-	http.HandleFunc("/potato", v1GetPotato)
+	http.HandleFunc("/potato", handler.V1GetPotato)
 
 	http.ListenAndServe(port, nil)
-}
-
-func v1GetPotato(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if r.Method == http.MethodGet {
-		w.Write([]byte("{\"content\": \"potato\"}"))
-		return
-	}
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	w.Write([]byte("{\"error\": \"method not allowed\"}"))
 }
