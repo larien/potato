1ea45c685ec4c5734f4123d3e17d975bbb48b560
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-12-26 19:21:54 -0300

    feat: remove GetPotato handler

--- handler/handler.go
+++ handler/handler.go
@@ -22,16 +22,6 @@ var (
 	lock    sync.RWMutex
 )
 
-func GetPotato(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if r.Method == http.MethodGet {
-		_, _ = w.Write([]byte("{\"content\": \"potato\"}"))
-		return
-	}
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	_, _ = w.Write([]byte("{\"error\": \"method not allowed\"}"))
-}
-
 func GetPotatos(w http.ResponseWriter, r *http.Request) {
 	response, err := json.Marshal(potatos)
 	if err != nil {
