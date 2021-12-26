891355209619d4affe6daf8c826af29096cdbb6a
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-12-25 23:45:50 -0300

    feat: include GH action steps with linting and testing (#21)
    * feat: include GH action script with linting
    
    * feat: include tests and coverage steps
    
    * fix: set bash as runner
    
    * fix: fix cov variable in coverage script
    
    * feat: add Dockerfile
    
    * feat: include workflows for push and pull request
    
    * fix: remove docker run step and add sequencial workflows
    
    * fix: move trigger to the correct file
    
    * fix: change token name
    
    * fix: remove trigger step
    
    * fix: remove dispatch
    
    * fix: fix syntax
    
    * fix: move to job
    
    * fix: change token
    
    * feat: include pr step for testig
    
    * fix: remove image id steps
    
    * fix: remove testing step
    
    * fix: rename workflow

---
+++ scripts/coverage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+COV_THRESHOLD=0
+
+echo "Running coverage tests..."
+totalCoverage=`go tool cover -func=coverage.out | grep total | grep -Eo '[0-9]+\.[0-9]+'`
+echo "Current test coverage: $totalCoverage%"
+if (( $(echo "$totalCoverage $COV_THRESHOLD" | awk '{print ($1 > $2)}') )); then
+    echo "OK"
+else
+    echo "Coverage is below threshold of $COV_THRESHOLD%"
+    exit 1
+fi
